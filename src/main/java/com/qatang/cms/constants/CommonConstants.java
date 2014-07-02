6ca6f4e581d0218573b3af3d72a2557a36dc72c6
JSHgithub <jinsheng@lecai.com>
2014-07-03 00:44:00 +0800

    完善用户的增删改查

--- src/main/java/com/qatang/cms/constants/CommonConstants.java
+++ src/main/java/com/qatang/cms/constants/CommonConstants.java
@@ -6,4 +6,5 @@ package com.qatang.cms.constants;
 public class CommonConstants {
     public final static String USER_SESSION_KEY = "user_session_key";
     public final static String KAPTCHA_SESSION_KEY = com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
+    public final static String USER_ID_KEY = "id";
 }
--- src/main/java/com/qatang/cms/controller/user/UserController.java
+++ src/main/java/com/qatang/cms/controller/user/UserController.java
@@ -2,14 +2,21 @@ package com.qatang.cms.controller.user;
 
 import com.qatang.cms.controller.BaseController;
 import com.qatang.cms.entity.user.User;
+import com.qatang.cms.enums.Gender;
+import com.qatang.cms.exception.validator.ValidateFailedException;
 import com.qatang.cms.form.user.UserForm;
 import com.qatang.cms.service.user.UserService;
+import com.qatang.cms.validator.impl.user.CreateUserValidator;
+import com.qatang.cms.validator.impl.user.UpdateUserValidator;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,6 +25,10 @@ import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class UserController extends BaseController {
+    @Autowired
+    private CreateUserValidator createUserValidator;
+    @Autowired
+    private UpdateUserValidator updateUserValidator;
     @Autowired
     private UserService userService;
 
@@ -38,12 +49,58 @@ public class UserController extends BaseController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(UserForm userForm) {
+    public String create(UserForm userForm, ModelMap modelMap) {
+        try {
+            createUserValidator.validate(userForm);
+        } catch (ValidateFailedException e) {
+            logger.error(e.getMessage(), e);
+            modelMap.addAttribute(ERROR_MESSAGE_KEY, e.getMessage());
+            return "failure";
+        }
+        User user = new User();
+        user.setUsername(userForm.getUsername());
+        user.setPassword(DigestUtils.md5Hex(userForm.getPassword()));
+        user.setName(userForm.getName());
+        if (!StringUtils.isEmpty(userForm.getEmail())) {
+            user.setEmail(userForm.getEmail());
+        } else {
+            user.setEmail(userForm.getUsername());
+        }
+        if (!StringUtils.isEmpty(userForm.getMobile())) {
+            user.setMobile(userForm.getMobile());
+        }
+        user.setCreatedTime(new Date());
+        userService.save(user);
         return "success";
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(Long id, UserForm userForm) {
+    public String update(Long id, UserForm userForm, ModelMap modelMap) {
+        if (id == null) {
+            logger.error("更新用户，id为空");
+            modelMap.addAttribute(ERROR_MESSAGE_KEY, "更新用户，id为空");
+            return "failure";
+        }
+        User user = userService.get(id);
+        if (user == null) {
+            logger.error("所要更新的用户不存在");
+            modelMap.addAttribute(ERROR_MESSAGE_KEY, "所要更新的用户不存在");
+            return "failure";
+        }
+        try {
+            updateUserValidator.validate(userForm);
+        } catch (ValidateFailedException e) {
+            logger.error(e.getMessage(), e);
+            modelMap.addAttribute(ERROR_MESSAGE_KEY, e.getMessage());
+            return "failure";
+        }
+        user.setUsername(userForm.getUsername());
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setMobile(userForm.getMobile());
+        user.setGender(Gender.get(Integer.parseInt(userForm.getGenderValue())));
+        user.setUpdatedTime(new Date());
+        userService.save(user);
         return "success";
     }
 
