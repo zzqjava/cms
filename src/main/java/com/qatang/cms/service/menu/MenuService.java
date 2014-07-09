08aa9134ba4271e421bd3df96921a20a1a873136
likunpeng11 <a785241128b@163.com>
2014-07-09 19:08:34 +0800

    Revert "Revert "菜单管理的增删改查""
    This reverts commit ca8bda321e58066d3a43b82c6b5847bebda0418b.

---
+++ src/main/java/com/qatang/cms/service/menu/MenuService.java
@@ -0,0 +1,37 @@
+package com.qatang.cms.service.menu;
+
+import com.qatang.cms.entity.menu.Menu;
+
+import java.util.List;
+
+/**
+ * Created by likunpeng on 2014/6/24.
+ */
+public interface MenuService {
+
+	/**
+	 *菜单列表
+	 * @return
+	 */
+	public List<Menu> getList();
+
+	/**
+	 * 根据菜单id查询
+	 * @param id
+	 * @return
+	 */
+	public Menu get(Long id);
+
+	/**
+	 * 保存
+	 * @param menu
+	 * @return
+	 */
+	public Menu save(Menu menu);
+
+	/**
+	 * 删除
+	 * @param id
+	 */
+	public void delete(Long id);
+}
